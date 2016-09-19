fc53650de36b302f90aff9311a9ff9c1c88d3973
TerabyteNation <terabytenation@gmail.com>
2016-09-19 13:22:11 -0400

    Added VAO/VBO drawing

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,11 @@
 package game;
 
 import graphics.Display;
+import graphics.Model;
+import graphics.ModelLoad;
+import graphics.Render;
 
+import org.lwjgl.opengl.GL;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
@@ -9,15 +13,29 @@ import static org.lwjgl.glfw.GLFW.glfwPollEvents;
 public class Main {
 
 	private static Display display = new Display();
+	private static ModelLoad modelLoad = new ModelLoad();
+	private static Render render = new Render();
+	
+	static float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f
+	};
 	
 	public static void main(String[] args) {
-		
 		display.init(1280, 720, "gel");
+		GL.createCapabilities();
+		
+		Model model = modelLoad.loadToVao(vertices);
 		
 		while(!glfwWindowShouldClose(display.glWindow)) {
+			render.load();
+			
+			render.render(model);
+			
 			glfwSwapBuffers(display.glWindow);
 			glfwPollEvents();
 		}
+		
+		modelLoad.cleanModel();
+		display.destroy();
 	}
 	
 }
